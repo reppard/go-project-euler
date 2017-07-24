de43aad2f01159b395e3e9a6e35793d7dca7f4ab
reppard <reppardwalker@gmail.com>
2017-07-23 22:05:42 -0400

    feat(): add output-helpers

---
+++ output-helpers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+const borderString = "//"
+
+func printBorderedMsg(message []string) {
+	printBar()
+	for i := 0; i < len(message); i++ {
+		fmt.Println(borderString, " ", message[i])
+	}
+	printBar()
+	fmt.Println("\n")
+}
+
+func printBar() {
+	s := ""
+
+	for i := 1; i <= 40; i++ {
+		s += borderString
+	}
+	fmt.Println(s)
+}
